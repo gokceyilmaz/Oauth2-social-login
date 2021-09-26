89ba1ebf58de9ac901a6689270aec60959e5f4a3
gokceyilmaz <gokceyilmaz.ce@gmail.com>
2021-09-26 16:02:47 +0300

    Initial commit

---
+++ src/main/java/com/gokce/nevitechauth2sociallogin/NevitechAuth2SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.gokce.nevitechauth2sociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevitechAuth2SocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NevitechAuth2SocialLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokce/nevitechauth2sociallogin/NevitechAuth2SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokce.nevitechauth2sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NevitechAuth2SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
